fcdc69f799fc099692ac48300fd908a303b4ebff
Serabass <serabass565@gmail.com>
2022-04-25 14:27:45 +0600

    1

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase {
+    /**
+     * @test
+     */
+    public function exampleTest() {
+        $this->assertTrue(true);
+    }
+}
